6c2dbfe24e4bcdc68e9ac45a83e58c5878d0ee77
Amanda Ualt <nobre@alunos.utfpr.edu.br>
2021-03-02 13:39:49 -0300

    Classe Element e Solos

---
+++ maven/src/main/java/calculo_solos/Solos.java
@@ -0,0 +1,33 @@
+package calculo_solos;
+
+public class Solos {
+	
+	
+//	private static double element( ){}
+	
+	private Element calculaSolos(int tipoSolo) {
+		Element elemento = new Element();
+		switch (tipoSolo) {
+			case 1:
+				elemento.setFosforo(9.0);
+				elemento.setPotassio(0.35);
+				elemento.setCalcio(6.0);
+				elemento.setMagnesio(1.5);
+				elemento.setEnxofre(9.0);
+				elemento.setAluminio(0);
+				elemento.sethAl(0);
+				return elemento;
+			case 2:
+				elemento.setFosforo(12.0);
+				elemento.setPotassio(0.25);
+				elemento.setCalcio(6.0);
+				elemento.setMagnesio(1.5);
+				elemento.setEnxofre(9.0);
+				elemento.setAluminio(0);
+				elemento.sethAl(0);
+				return elemento;
+			default:
+				return elemento;
+		}
+	}
+}
