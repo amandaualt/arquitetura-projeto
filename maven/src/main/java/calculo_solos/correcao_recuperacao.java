e8a62cbf1585a9d849cad65f1d33a4323daadbaf
Amanda Ualt <nobre@alunos.utfpr.edu.br>
2021-03-30 13:02:19 -0300

    criação de gets e sets de fontes de FOSFORO

---
+++ maven/src/main/java/calculo_solos/correcao_recuperacao.java
@@ -0,0 +1,108 @@
+package calculo_solos;
+
+public class correcao_recuperacao {
+	
+	//fosforo
+	private double superFofatoSimples;
+	private double superFofatoTriplo;
+	private double map;
+	private double dap;
+	private double yoorin;
+	private double fosfatoArad;
+	private double fosfatoGafsa;
+	private double fosfatoDaoui;
+	private double fosfPatosMinas;
+	private double excoriaThomas;
+	private double acidoFosforico;
+	private double multifMag;
+
+	
+	//fosforo
+	public double getSuperFofatoSimples(){
+		return superFofatoSimples;
+	}
+	public void setSuperFofatoSimples(double superFofatoSimples) {
+		this.superFofatoSimples = superFofatoSimples;
+	}
+	
+	public double getSuperFofatoTriplo(){
+		return superFofatoTriplo;
+	}
+	public void setDataProdutor(double superFofatoTriplo) {
+		this.superFofatoTriplo = superFofatoTriplo;
+	}
+	
+	public double getMap(){
+		return map;
+	}
+	public void setMap(double map) {
+		this.map = map;
+	}
+	
+	public double getDap(){
+		return dap;
+	}
+	public void setDap(double dap) {
+		this.dap = dap;
+	}
+	
+	public double getYoorin(){
+		return yoorin;
+	}
+	public void setYoorin(double yoorin) {
+		this.yoorin = yoorin;
+	}
+	
+	public double getFosfatoArad(){
+		return fosfatoArad;
+	}
+	public void setFosfatoArad (double fosfatoArad) {
+		this.fosfatoArad = fosfatoArad;
+	}
+	
+	public double getFosfatoGafsa(){
+		return fosfatoGafsa;
+	}
+	public void setFosfatoGafsa(double fosfatoGafsa) {
+		this.fosfatoGafsa = fosfatoGafsa;
+	}
+	
+	public double getFosfatoDaoui(){
+		return fosfatoDaoui;
+	}
+	public void setFosfatoDaoui(double fosfatoDaoui) {
+		this.fosfatoDaoui = fosfatoDaoui;
+	}
+	
+	
+	public double getFosfPatosMinas() {
+		return fosfPatosMinas;
+	}
+	public void setFosfPatosMinas(double fosfPatosMinas) {
+		 this.fosfPatosMinas = fosfPatosMinas;
+	}
+	
+	
+	public double getExcoriaThomas(){
+		return excoriaThomas;
+	}
+	public void setExcoriaThomaso(double excoriaThomas) {
+		this.excoriaThomas = excoriaThomas;
+	}
+	
+	
+	public double getAcidoFosforico(){
+		return acidoFosforico;
+	}
+	public void setAcidoFosforico(double acidoFosforico) {
+		this.acidoFosforico = acidoFosforico;
+	}
+	
+	
+	public double getMultifMag(){
+		return multifMag;
+	}
+	public void setMultifMag(double multifMag) {
+		this.multifMag = multifMag;
+	}
+}
