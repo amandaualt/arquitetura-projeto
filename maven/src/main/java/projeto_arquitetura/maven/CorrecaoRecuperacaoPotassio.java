0004a905e4517e7dd8251e4056db18bf93c3784e
Amanda Ualt <nobre@alunos.utfpr.edu.br>
2021-04-07 11:40:03 -0300

    terminando calculos quantidade a aplicar

--- maven/src/main/java/projeto_arquitetura/maven/CorrecaoRecuperacaoPotassio.java
+++ maven/src/main/java/projeto_arquitetura/maven/CorrecaoRecuperacaoPotassio.java
@@ -3,24 +3,22 @@ package projeto_arquitetura.maven;
 public class CorrecaoRecuperacaoPotassio {
 
     private double teorPotCtcDesj;
-    private int fontPotUsar;
     private double valorPotassioRecebido;
-    
-    public CorrecaoRecuperacaoPotassio(int fontPotUsar, double valorPotassioRecebido, double teorPotCtcDesj) {
+
+    public CorrecaoRecuperacaoPotassio(double valorPotassioRecebido, double teorPotCtcDesj) {
         this.teorPotCtcDesj = teorPotCtcDesj;
         this.valorPotassioRecebido = valorPotassioRecebido;
-        this.fontPotUsar = fontPotUsar;
     }
 
-    public double calculoCTCMolPotassio(double ctcCmol){
-        return valorPotassioRecebido/ctcCmol * 10;
+    public double calculoCTCCmolPotassio(double ctcCmol) {
+        return valorPotassioRecebido / ctcCmol * 100;
     }
 
     public double necessidadePotassioAdd(double ctcCmolPotassio) {
         return (valorPotassioRecebido * teorPotCtcDesj / ctcCmolPotassio) - valorPotassioRecebido;
     }
 
-    public double calculoCmolcCm3KMgDm3(double necessidadeKAdd) {
+    public double calculoCmolCm3KMgDm3(double necessidadeKAdd) {
         if (necessidadeKAdd < 0.01) {
             return 0.0;
         }
@@ -35,8 +33,11 @@ public class CorrecaoRecuperacaoPotassio {
         return mgDm3KgHa * 1.2;
     }
 
-    public double calculoConsiderandoEficPotassio(double kgHaKK2O, double consideraEficienciaPotassio) {
-        return kgHaKK2O * 100 / consideraEficienciaPotassio / 10;
+    public double calculoConsiderandoEficaciaPotassio(double kgHaKK2O, double consideraEficienciaPotassio) {
+        return kgHaKK2O * 100 / consideraEficienciaPotassio / 100;
     }
 
+    public double calculoQuantidadeAplicar(double resultadoConsiderandoEficiaciaPotassio, double enumPotassio){
+        return resultadoConsiderandoEficiaciaPotassio * 100 / enumPotassio;
+    }
 }
--- maven/src/test/java/projeto_arquitetura/maven/RecCorrecaoTest.java
+++ maven/src/test/java/projeto_arquitetura/maven/RecCorrecaoTest.java
@@ -1,45 +1,56 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package projeto_arquitetura.maven;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-/**
- *
- * @author amand
- */
 public class RecCorrecaoTest {
-    
-    public RecCorrecaoTest() {
-    }
-    
-    @BeforeAll
-    public static void setUpClass() {
-    }
-    
-    @AfterAll
-    public static void tearDownClass() {
-    }
-    
-    @BeforeEach
-    public void setUp() {
-    }
-    
-    @AfterEach
-    public void tearDown() {
+
+    @Test
+    public void testCalculoCTCCmolPotassio() {
+        assertEquals(1.163692785,
+                new CorrecaoRecuperacaoPotassio(0.15, 0.0).calculoCTCCmolPotassio(12.89), 0.1);
     }
 
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
+    @Test
+    public void testNecessidadePotassioAdd() {
+        assertEquals(0.2379,
+                new CorrecaoRecuperacaoPotassio(0.15, 3.0).necessidadePotassioAdd(1.16), 0.1);
+    }
+
+    @Test
+    public void testCalculoCmolCm3KMgDm3() {
+        assertEquals(89.93,
+                new CorrecaoRecuperacaoPotassio(0.0, 0.0).calculoCmolCm3KMgDm3(0.23), 0.1);
+    }
+
+    @Test
+    public void testCalculoMgDm3KgHa() {
+        assertEquals(179.86,
+                new CorrecaoRecuperacaoPotassio(0.0, 0.0).calculoMgDm3KgHa(89.93), 0.1);
+    }
+
+    @Test
+    public void testCalculoKgHaKK2O() {
+        assertEquals(215.832,
+                new CorrecaoRecuperacaoPotassio(0.0, 0.0).calculoKgHaKK2O(179.86), 0.1);
+    }
+
+    @Test
+    public void testCalculoConsiderandoEficaciaPotassio() {
+        assertEquals(253.92,
+                new CorrecaoRecuperacaoPotassio(0.0, 0.0).calculoConsiderandoEficaciaPotassio(215.832, 0.85), 0.1);
+    }
+
+    @Test
+    public void testCalculoQuantidadeAplicar() {
+        assertEquals(437.79,
+                new CorrecaoRecuperacaoPotassio(0.0, 0.0).calculoQuantidadeAplicar(253.92, EnumPotassio.cloreto_de_potassio.valorElement()), 0.1);
+        assertEquals(488.30,
+                new CorrecaoRecuperacaoPotassio(0.0, 0.0).calculoQuantidadeAplicar(253.92, EnumPotassio.sulfato_de_potassio.valorElement()), 0.1);
+        assertEquals(1154.18,
+                new CorrecaoRecuperacaoPotassio(0.0, 0.0).calculoQuantidadeAplicar(253.92, EnumPotassio.sulfato_de_potassio_e_magnesio.valorElement()), 0.1);
+        assertEquals(577.09,
+                new CorrecaoRecuperacaoPotassio(0.0, 0.0).calculoQuantidadeAplicar(253.92, EnumPotassio.nitrato_de_potassio.valorElement()), 0.1);
+
+    }
 }
