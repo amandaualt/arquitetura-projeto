251e59d4ac5885e2e54a09c997c752e7593cebcd
Amanda Ualt <nobre@alunos.utfpr.edu.br>
2021-04-06 22:02:00 -0300

    adicionando calculos em potassio

--- maven/src/main/java/projeto_arquitetura/maven/RecCorrecaoPotassio.java
+++ maven/src/main/java/projeto_arquitetura/maven/RecCorrecaoPotassio.java
@@ -8,14 +8,41 @@ public class RecCorrecaoPotassio {
     private int fontPotUsar;
     private double valorPotassioRecebido;
 
-    public RecCorrecaoPotassio(int fontPotUsar, double valorPotassioRecebido) {
+    
+    
+    public RecCorrecaoPotassio(int fontPotUsar, double valorPotassioRecebido, double teorPotCtcDesj) {
+        this.teorPotCtcDesj = teorPotCtcDesj;
         this.valorPotassioRecebido = valorPotassioRecebido;
         this.fontPotUsar = fontPotUsar;
     }
-    public double calcCTCMolPotassio(){
+    public double calculoCTCMolPotassio(){
         return valorPotassioRecebido/equiCTC.somaCtcCmol() * 10;
     }
-    public double calcFontPotassioUsar() {
+    
+    public double necessidadeKAdd(){
+        return (valorPotassioRecebido*teorPotCtcDesj/calculoCTCMolPotassio())-valorPotassioRecebido;
+    }
+   
+    public double calculocmolcCm3KMgDm3(){
+        if (necessidadeKAdd() < 0.01){
+            return 0.0;
+        }
+        return necessidadeKAdd() * 39.1 * 10;
+    }
+    
+    public double calculoMgDm3KgHa(){
+        return calculocmolcCm3KMgDm3() * 2;
+    }
+    
+    public double calculoKgHaKK2O(){
+        return calculoMgDm3KgHa() * 1.2;
+    }
+    
+    public double calculoConsiderandoEficPotassio(){
+        return calculoKgHaKK2O() * 100 / 0.85 / 10;
+    }
+    
+    public double calculoFontPotassioUsar() {
         switch (fontPotUsar) {
             case 1:
                 return 58.0;
