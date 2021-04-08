d1c8861daf55830a79b2395932231871e161aa45
Amanda Ualt <nobre@alunos.utfpr.edu.br>
2021-04-07 23:07:28 -0300

    calculos e testes fosforo

--- maven/src/main/java/projeto_arquitetura/maven/CorrecaoRecuperacaoFosforo.java
+++ maven/src/main/java/projeto_arquitetura/maven/CorrecaoRecuperacaoFosforo.java
@@ -2,6 +2,8 @@ package projeto_arquitetura.maven;
 
 public class CorrecaoRecuperacaoFosforo {
 
+    private EnumFosforo elementosFosforo;
+
     public double calculoNessecidadeAdd(double teorFosforoAtingir, double fosforoRecebido) {
         return teorFosforoAtingir - fosforoRecebido;
     }
@@ -24,15 +26,53 @@ public class CorrecaoRecuperacaoFosforo {
     public double calculoNecessitamosFosforo(double transformandoKgHaFosforoKgHaP2O5, double eficienciaFosforo) {
         return transformandoKgHaFosforoKgHaP2O5 * 100 / eficienciaFosforo / 100;
     }
-    
-    public double calculoQuantidadeAplicar(double calculoNecessitamosFosforo, double fonteFosforoUtilizar){
+
+    public double calculoQuantidadeAplicar(double calculoNecessitamosFosforo, double fonteFosforoUtilizar) {
         return calculoNecessitamosFosforo * 100 / fonteFosforoUtilizar;
     }
-    
-       public double calculoCustoPorHa(double valorFonteFosfoto, double quantidadeAplicar){
-        if(valorFonteFosfoto != 0){
-            return ((valorFonteFosfoto*(quantidadeAplicar * 2.42))/1000)/2.42;
+
+    public double multiplicaQuantidadeAplicar(double quantidadeAplicar) {
+        return quantidadeAplicar * 2.42;
+    }
+
+    public double calculoCustoPorHa(double valorFonteFosfoto, double multiplicaQuantidadeAplicar) {
+        if (valorFonteFosfoto != 0) {
+            return ((valorFonteFosfoto * multiplicaQuantidadeAplicar) / 1000) / 2.42;
         }
         return 0.0;
+    }
+
+    public double verificaFontPotassioSuperSimplesOuMultMagnesiano(double multiplicaQuantidadeAplicar) {
+        switch (elementosFosforo) {
+            case SUPERFOSTATO_SIMPLES:
+                return multiplicaQuantidadeAplicar * 0.1;
+            case MULTIF_MAGNESIANO:
+                return multiplicaQuantidadeAplicar * 0.11;
+            default:
+                return 0.0;
+        }
+    }
+
+    public double calculaM23(double verificaFontPotassioSuperSimplesOuMultMagnesiano) {
+        return verificaFontPotassioSuperSimplesOuMultMagnesiano * 2.42;
+    }
+
+    public double calculaN21(double calculoQuantidadeAplicar) {
+        return calculoQuantidadeAplicar * 0.15;
+    }
+
+    public String verificaEnxofreOuMagnesio(double multiplicaQuantidadeAplicar) {
+        switch (elementosFosforo) {
+            case SUPERFOSTATO_SIMPLES:
+                return ("Enxofre");
+
+            case MULTIF_MAGNESIANO:
+                return ("Enxofre");
+
+            case YOORIN:
+                return ("Magnésio");
+            default:
+                return null;
+        }
     }
 }
